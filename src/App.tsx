51855752e9649023ceab744c80c60237e59d94c8
kinderhead <kinderhead512@gmail.com>
2023-04-30 17:43:45 -0400

    Some changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
+import { PropsWithChildren } from 'react'
 import './App.css'
 import Content from './Content'
 import fileUrl from './assets/bg.png'
 
-export default function App()
+export default function App(props: PropsWithChildren<object>)
 {
     return (
         <>
             <img className="side left" src={fileUrl}></img>
             <img className="side right" src={fileUrl}></img>
             <div className="center">
-                <Content></Content>
+                <Content>{props.children}</Content>
             </div>
         </>
     )
--- src/Content.tsx
+++ src/Content.tsx
@@ -1,24 +1,34 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import { LinkContainer } from 'react-router-bootstrap';
 
 import './Content.css'
+import { PropsWithChildren } from 'react';
 
-export default function Content()
+export default function Content(props: PropsWithChildren<object>)
 {
     return (
         <>
             <Navbar bg="light" expand="lg" sticky="top">
                 <Container fluid>
-                    <Navbar.Brand href="/">MSSM French 211</Navbar.Brand>
+                    <LinkContainer to="/">
+                        <Navbar.Brand>MSSM French 211</Navbar.Brand>
+                    </LinkContainer>
                     <Navbar.Toggle aria-controls="basic-navbar-nav"/>
                     <Navbar.Collapse id="basic-navbar-nav">
-                        <Nav className="me-auto">
-                            <Nav.Link href="#bruh">Topic 1</Nav.Link>
+                        <Nav>
+                            <LinkContainer to="/first">
+                                <Nav.Link>Topic 1</Nav.Link>
+                            </LinkContainer>
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
             </Navbar>
+
+            <div className="content-body">
+                {props.children}
+            </div>
         </>
     );
 }
