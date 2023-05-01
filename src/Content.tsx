43295863c5c87063fffc4135e832bc3e10ae8322
kinderhead <kinderhead512@gmail.com>
2023-04-30 20:19:11 -0400

    Nav headers

--- src/Content.tsx
+++ src/Content.tsx
@@ -5,6 +5,7 @@ import { LinkContainer } from 'react-router-bootstrap';
 
 import './Content.css'
 import { PropsWithChildren } from 'react';
+import { NavDropdown } from 'react-bootstrap';
 
 export default function Content(props: PropsWithChildren<object>)
 {
@@ -17,10 +18,30 @@ export default function Content(props: PropsWithChildren<object>)
                     </LinkContainer>
                     <Navbar.Toggle aria-controls="basic-navbar-nav"/>
                     <Navbar.Collapse id="basic-navbar-nav">
-                        <Nav>
-                            <LinkContainer to="first">
-                                <Nav.Link>Topic 1</Nav.Link>
+                        <Nav className="me-auto">
+                            <LinkContainer to="/french2-site/directs-indirects">
+                                <Nav.Link>Les pronoms objets directs et indirects</Nav.Link>
                             </LinkContainer>
+                            <NavDropdown title="Verb tenses/moods">
+                                <LinkContainer to="/french2-site/passé-composé-avoir">
+                                    <NavDropdown.Item>Le passé composé avec avoir</NavDropdown.Item>
+                                </LinkContainer>
+                                <LinkContainer to="/french2-site/passé-composé-être">
+                                    <NavDropdown.Item>Le passé composé avec être</NavDropdown.Item>
+                                </LinkContainer>
+                                <LinkContainer to="/french2-site/imparfait">
+                                    <NavDropdown.Item>L’imparfait</NavDropdown.Item>
+                                </LinkContainer>
+                                <LinkContainer to="/french2-site/conditionnel">
+                                    <NavDropdown.Item>Le conditionnel</NavDropdown.Item>
+                                </LinkContainer>
+                                <LinkContainer to="/french2-site/subjonctif">
+                                    <NavDropdown.Item>Le subjonctif</NavDropdown.Item>
+                                </LinkContainer>
+                                <LinkContainer to="/french2-site/futur">
+                                    <NavDropdown.Item>Le futur simple</NavDropdown.Item>
+                                </LinkContainer>
+                            </NavDropdown>
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
