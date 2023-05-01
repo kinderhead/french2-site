9fe4e92f8fc2f8144aa23c6508ac9941ffc8ac3e
kinderhead <kinderhead512@gmail.com>
2023-05-01 12:51:20 -0400

    Error fix

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import { BrowserRouter, HashRouter, Route, Routes } from 'react-router-dom'
+import { HashRouter, Route, Routes } from 'react-router-dom'
 import fileUrl from './assets/cover.jpg'
 import RemoteMarkdown from './Markdown.tsx'
 
